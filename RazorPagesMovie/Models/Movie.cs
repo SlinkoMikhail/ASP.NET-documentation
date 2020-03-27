5235769f714394ec90c797b566a2808e8b9bdb63
Mikhail Slinko <slinkom@gmail.com>
2020-03-27 07:10:20 +0300

    Added filter and search

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorPagesMovie.Models
 {
@@ -8,9 +9,11 @@ namespace RazorPagesMovie.Models
         public int Id { get; set; }
         public string Title { get; set; }
 
+        [Display(Name = "Released Date")]
         [DataType(DataType.Date)]
         public DateTime ReleasedDate { get; set; }
         public string Genre { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
 
     }
